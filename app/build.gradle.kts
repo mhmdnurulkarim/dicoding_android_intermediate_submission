9a547a14c581ae01e6ce4bfdf1ee07c2a4f9b430
karim.2501 <karimmitra0@gmail.com>
2023-10-30 17:26:57 +0700

    Update Readme.md

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
     id("kotlin-parcelize")
+
 }
 
 android {
@@ -17,7 +19,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
